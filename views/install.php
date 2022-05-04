3c30b4f2690a26cabbe573298f88e16d60fa057a
Timi Olusegun <timiddon97@gmail.com>
2022-05-04 21:29:00 +0100

    Use Bootstrap CDN

--- views/install.php
+++ views/install.php
@@ -5,10 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GASKIYA | Sign In</title>
-    <link rel="stylesheet" href="public/bootstrap/css/bootstrap.css">
+    <title>GACOSCA | Sign In</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+        crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/site.css">
-    <script src="public/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+        crossorigin="anonymous">
+    </script>
 </head>
 
 <body class="h-100 d-flex">
--- views/vote.php
+++ views/vote.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GASKIYA Elections | Vote</title>
-    <link rel="stylesheet" href="public/bootstrap/css/bootstrap.css">
+    <title>GACOSCA Elections | Vote</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+        crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/site.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" 
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" 
+        crossorigin="anonymous">
+    </script>
     <script src="public/js/site.js"></script>
-    <script src="public/bootstrap/bootstrap.min.js"></script>
 </head>
 
 <body class="">
